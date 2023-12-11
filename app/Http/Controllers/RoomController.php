ccb61c8e071ad6c6ba3bf2e2fd31a2ac3f96c421
howestjorn <125253286+howestjorn@users.noreply.github.com>
2023-12-11 15:06:30 +0100

    adding room id to timestamps

--- app/Http/Controllers/RoomController.php
+++ app/Http/Controllers/RoomController.php
@@ -10,20 +10,22 @@ use Carbon\Carbon;
 class RoomController extends Controller
 {
         public function checkRoomAvailability(){
-        $meetingtimes = Meeting::select("start_date", "end_date")->get();
+        $meetingtimes = Meeting::select("start_date", "end_date", "room_id")->get();
         $data = array();
         $i = 0;
         $ini = carbon::today();
         $date = $ini->toDateString();
         foreach ($meetingtimes as $t) {
             $day = carbon::parse($meetingtimes[$i]->start_date)->format('Y-m-d');
-            if ($day === $date) {
-                $interval = carbon::parse($meetingtimes[$i]->start_date)->format('H:i:s')."-".carbon::parse($meetingtimes[$i]->end_date)->format('H:i:s');
-                $data[] = $interval;
+            if ($day === "2025-01-01") {
+                $test = (object) [
+                    'room' => $meetingtimes[$i]->room_id,
+                    'timestamp' => carbon::parse($meetingtimes[$i]->start_date)->format('H:i:s')."-".carbon::parse($meetingtimes[$i]->end_date)->format('H:i:s'),
+                ];
+                $data[] = $test;
             }
             $i += 1;
         }
         return $data;
-        
     }
 }
