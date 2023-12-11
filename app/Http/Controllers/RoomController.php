13e4c891bc0d60c3d3959cb12a78d13d66f33b12
howestjorn <125253286+howestjorn@users.noreply.github.com>
2023-12-11 15:52:25 +0100

    changed roomavailibitly function to work with param

--- app/Http/Controllers/RoomController.php
+++ app/Http/Controllers/RoomController.php
@@ -6,10 +6,11 @@ use Illuminate\Http\Request;
 use App\Models\Room;
 use App\Models\Meeting;
 use Carbon\Carbon;
+use App\Http\Requests\MeetingCheckRequest;
 
 class RoomController extends Controller
 {
-        public function checkRoomAvailability(){
+    function checkRoomAvailability(MeetingCheckRequest $request){
         $meetingtimes = Meeting::select("start_date", "end_date", "room_id")->get();
         $data = array();
         $i = 0;
@@ -17,10 +18,11 @@ class RoomController extends Controller
         $date = $ini->toDateString();
         foreach ($meetingtimes as $t) {
             $day = carbon::parse($meetingtimes[$i]->start_date)->format('Y-m-d');
-            if ($day === "2025-01-01") {
+            if ($day === $request->date/*"2025-01-01"*/) {
                 $test = (object) [
                     'room' => $meetingtimes[$i]->room_id,
-                    'timestamp' => carbon::parse($meetingtimes[$i]->start_date)->format('H:i:s')."-".carbon::parse($meetingtimes[$i]->end_date)->format('H:i:s'),
+                    'timestamp_start' => carbon::parse($meetingtimes[$i]->start_date)->format('H:i:s'),
+                    'timestamp_end' => carbon::parse($meetingtimes[$i]->end_date)->format('H:i:s'),
                 ];
                 $data[] = $test;
             }
