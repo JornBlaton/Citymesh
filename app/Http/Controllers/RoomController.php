bc31802871a290b8b86ce8daf0f9667653eb19e2
howestjorn <125253286+howestjorn@users.noreply.github.com>
2023-12-11 14:29:43 +0100

    pulling timestampes and sorting out current day meetings

--- app/Http/Controllers/RoomController.php
+++ app/Http/Controllers/RoomController.php
@@ -11,6 +11,19 @@ class RoomController extends Controller
 {
         public function checkRoomAvailability(){
         $meetingtimes = Meeting::select("start_date", "end_date")->get();
-        return carbon::parse($meetingtimes[0]->start_date)->format('Y-m-d H:i:s');
+        $data = array();
+        $i = 0;
+        $ini = carbon::today();
+        $date = $ini->toDateString();
+        foreach ($meetingtimes as $t) {
+            $day = carbon::parse($meetingtimes[$i]->start_date)->format('Y-m-d');
+            if ($day === $date) {
+                $data[] = carbon::parse($meetingtimes[$i]->start_date)->format('H:i:s');
+                $data[] = carbon::parse($meetingtimes[$i]->end_date)->format('H:i:s');
+            }
+            $i += 1;
+        }
+        return $data;
+        
     }
 }
