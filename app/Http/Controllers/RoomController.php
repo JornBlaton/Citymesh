ba595b0b975ea3fd5380763aeb3abd8915cd1fff
howestjorn <125253286+howestjorn@users.noreply.github.com>
2023-12-11 14:04:41 +0100

    adding carbon plugin and calling timestamps

--- app/Http/Controllers/RoomController.php
+++ app/Http/Controllers/RoomController.php
@@ -5,11 +5,12 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Room;
 use App\Models\Meeting;
+use Carbon\Carbon;
 
 class RoomController extends Controller
 {
         public function checkRoomAvailability(){
         $meetingtimes = Meeting::select("start_date", "end_date")->get();
-        return response()->json($meetingtimes);
+        return carbon::parse($meetingtimes[0]->start_date)->format('Y-m-d H:i:s');
     }
 }
